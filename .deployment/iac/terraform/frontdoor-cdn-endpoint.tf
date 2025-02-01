ab3618b064e08a5432bcb0adf1a255db3e6b914e
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-01 19:42:08 +0000

    Merged PR 279: updates to afd caching and moving dploy files around
    updates to afd caching and moving dploy files around
    
    Related work items: #319

--- .deployment/iac/terraform/frontdoor-cdn-endpoint.tf
+++ .deployment/iac/terraform/frontdoor-cdn-endpoint.tf
@@ -30,7 +30,7 @@ resource "azurerm_cdn_frontdoor_origin" "kelso_storybook_cdn_origin" {
   name                              = "afdo-${var.sub}-${var.region}-${var.environment}-${var.domain}-${var.sequence}"
   cdn_frontdoor_origin_group_id     = azurerm_cdn_frontdoor_origin_group.kelso_storybook_cdn_origin_group.id
   enabled                           = true
-  certificate_name_check_enabled    = false
+  certificate_name_check_enabled    = true
   host_name                         = azurerm_storage_account.storybook_web_storage_account.primary_web_host
   http_port                         = 80
   https_port                        = 443
@@ -39,22 +39,65 @@ resource "azurerm_cdn_frontdoor_origin" "kelso_storybook_cdn_origin" {
   weight                            = 1000
 }
 
+
+resource "azurerm_cdn_frontdoor_rule_set" "kelso_storybook_cdn_ruleset" {
+  name                     = "kelsostorybook"
+  cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.global_shared_cdn.id
+}
+
+resource "azurerm_cdn_frontdoor_rule" "kelso_storybook_cdn_ruleset_caching_rule" {
+  name                        = "cachingrule"
+  cdn_frontdoor_rule_set_id   = azurerm_cdn_frontdoor_rule_set.kelso_storybook_cdn_ruleset.id
+  order                       = 1
+  behavior_on_match           = "Continue"
+
+  actions {
+    route_configuration_override_action {
+      query_string_caching_behavior = "UseQueryString"
+      cache_behavior                = "OverrideAlways"
+      cache_duration                = "30.00:00:00"
+      compression_enabled           = true
+    }
+  }
+
+  depends_on = [
+    azurerm_cdn_frontdoor_origin_group.kelso_storybook_cdn_origin_group,
+    azurerm_cdn_frontdoor_origin.kelso_storybook_cdn_origin
+  ]
+}
+
+
+
 resource "azurerm_cdn_frontdoor_route" "kelso_storybook_cdn_route" {
   name                          = "afdr-${var.sub}-${var.region}-${var.environment}-${var.domain}-${var.sequence}"
   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.kelso_storybook_cdn_endpoint.id
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.kelso_storybook_cdn_origin_group.id
   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.kelso_storybook_cdn_origin.id]
+  cdn_frontdoor_rule_set_ids    = [azurerm_cdn_frontdoor_rule_set.kelso_storybook_cdn_ruleset.id]
   enabled                       = true
+  link_to_default_domain        = true
 
   forwarding_protocol    = "MatchRequest"
   https_redirect_enabled = false
   patterns_to_match      = ["/*"]
   supported_protocols    = ["Https"]
 
-  link_to_default_domain          = true
-
   cache {
-    query_string_caching_behavior = "IgnoreQueryString"
-    compression_enabled           = false
+    query_string_caching_behavior = "UseQueryString"
+    compression_enabled = true
+    content_types_to_compress = [
+      "application/eot",
+      "application/font",
+      "application/javascript",
+      "application/json",
+      "application/xml",
+      "image/svg+xml",
+      "text/css",
+      "text/csv",
+      "text/html",
+      "text/javascript",
+      "text/plain",
+      "text/xml"
+    ]
   }
 }
